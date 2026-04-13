1d906918d8cc18ba092d1ac2df56e7e33a5ee448
Dev | Lucas Gomes <168451940+CLKlucas@users.noreply.github.com>
2026-04-13 10:50:09 -0300

    Teste System.IN

---
+++ src/test/java/testeSIN.java
@@ -0,0 +1,30 @@
+import br.com.topcar.io.PecaInputStream;
+import br.com.topcar.model.Peca;
+
+public class testeSIN {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Digite os dados exatamente no formato esperado pelo stream:");
+            System.out.println("qtd_pecas");
+            System.out.println("id");
+            System.out.println("nome");
+            System.out.println("valor");
+            System.out.println("data");
+            System.out.println("nomeCarro");
+            System.out.println("anoCarro");
+            System.out.println("modeloCarro");
+
+            PecaInputStream entrada = new PecaInputStream(System.in);
+            Peca[] pecas = entrada.readFromTerminal();
+
+            System.out.println("\nPeças lidas com sucesso:");
+            for (Peca p : pecas) {
+                System.out.println(p);
+            }
+
+        } catch (Exception e) {
+            System.out.println("Erro ao ler do System.in: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
